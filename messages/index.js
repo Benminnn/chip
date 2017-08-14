ab7713c809235974bd343ff2e47d9feccba141fe
benminnn <benhodes@smetna.com>
2017-08-13 23:38:38 -0700

    cleanup

--- messages/index.js
+++ messages/index.js
@@ -43,7 +43,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 //.matches('<yourIntent>') //See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 
 intents.onDefault((session) => {
-    voicebox = 'Not sure what to say, sorry.';
+    voicebox = 'sure';
     var voice_mp3 = get_voice(voicebox);
     voicebox = "";
 
